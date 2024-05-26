47bdd237659754a71935334ef094a21178e08a29
Ben Simms <ben@bensimms.moe>
2024-05-26 17:01:00 +0200

    Make time driver public

--- embassy-stm32/src/rcc/mod.rs
+++ embassy-stm32/src/rcc/mod.rs
@@ -39,13 +39,13 @@ use crate::time::Hertz;
 /// Must be written within a critical section
 ///
 /// May be read without a critical section
-pub(crate) static mut REFCOUNT_STOP1: u32 = 0;
+pub static mut REFCOUNT_STOP1: u32 = 0;
 
 #[cfg(feature = "low-power")]
 /// Must be written within a critical section
 ///
 /// May be read without a critical section
-pub(crate) static mut REFCOUNT_STOP2: u32 = 0;
+pub static mut REFCOUNT_STOP2: u32 = 0;
 
 /// Frozen clock frequencies
 ///
